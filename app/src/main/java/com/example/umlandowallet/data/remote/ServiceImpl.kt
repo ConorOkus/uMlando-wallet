03746369e24c3670342e3b3c7dbdcd919e04d014
Conor Okus <conor.okus@hotmail.co.uk>
2022-07-05 20:32:28 +0100

    Update to latest version

--- app/src/main/java/com/example/umlandowallet/data/remote/ServiceImpl.kt
+++ app/src/main/java/com/example/umlandowallet/data/remote/ServiceImpl.kt
@@ -9,18 +9,20 @@ import java.net.InetSocketAddress
 
 class ServiceImpl(private val client: HttpClient) : Service {
     override suspend fun getlatestBlockHash(): String {
-        return client.get("https://blockstream.info/testnet/api/blocks/tip/hash")
+        return client.get("http://10.0.2.2:3002/blocks/tip/hash")
     }
 
     override suspend fun getlatestBlockHeight(): Int {
-        return client.get("https://blockstream.info/testnet/api/blocks/tip/height")
+        return client.get("http://10.0.2.2:3002/blocks/tip/height")
     }
 
     override suspend fun connectPeer(pubkeyHex: String, hostname: String, port: Int): Boolean {
         println("LDK: attempting to connect to peer $pubkeyHex")
         return try {
-            Global.nioPeerHandler!!.connect(pubkeyHex.hexStringToByteArray(),
-                InetSocketAddress(hostname, port), 5000)
+            Global.nioPeerHandler!!.connect(
+                pubkeyHex.hexStringToByteArray(),
+                InetSocketAddress(hostname, port), 5555
+            )
             println("LDK: successfully connected to peer $pubkeyHex")
             true
         } catch (e: IOException) {
